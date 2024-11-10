73471822bb77245f1698f5e4c6f5270249c5b15b
webdevcodeblue <blueboy2314@gmail.com>
2024-11-10 13:23:45 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+(function ($) {
+  'use strict';
+
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($('#spinner').length > 0) {
+        $('#spinner').removeClass('show');
+      }
+    }, 1);
+  };
+  spinner();
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+    } else {
+      $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+  // Testimonials carousel
+  $('.testimonial-carousel').owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    items: 1,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>',
+    ],
+  });
+})(jQuery);
+
+//Navbar hambureger CLOSE
+document.querySelectorAll('.navbar-nav .nav-link').forEach((item) => {
+  item.addEventListener('click', () => {
+    const navbarCollapse = document.getElementById('navbarCollapse');
+    if (window.getComputedStyle(navbarCollapse).display === 'block') {
+      const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+        toggle: false,
+      });
+      bsCollapse.hide();
+    }
+  });
+});
